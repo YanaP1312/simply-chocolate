4fca6d0cbc303aab8614435a590c4e4513030a2c
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-04-22 18:12:06 +0300

    add no-scroll and toast for form

--- js/modal-review.js
+++ js/modal-review.js
@@ -5,12 +5,28 @@ const closeModalBtn = document.querySelector(".form-close-btn");
 
 const openModal = () => {
   modalOverlay.classList.add("is-open");
+  document.body.classList.add("no-scroll");
 };
 
 const closeModal = () => {
   modalOverlay.classList.remove("is-open");
+  document.body.classList.remove("no-scroll");
   openModalBtn.blur();
 };
+
+function showToast(message, duration = 3000) {
+  const toast = document.createElement("div");
+  toast.className = "toast";
+  toast.textContent = message;
+  document.body.appendChild(toast);
+
+  setTimeout(() => toast.classList.add("show"), 100);
+
+  setTimeout(() => {
+    toast.classList.remove("show");
+    setTimeout(() => toast.remove(), 400);
+  }, duration);
+}
 
 openModalBtn.addEventListener("click", openModal);
 closeModalBtn.addEventListener("click", closeModal);
@@ -25,4 +41,17 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     closeModal();
   }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".modal-form");
+
+  if (!form) return;
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    showToast("Thank you for review!");
+    closeModal();
+    form.reset();
+  });
 });
